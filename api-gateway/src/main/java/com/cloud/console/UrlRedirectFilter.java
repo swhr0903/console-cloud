e6987c50c075c2ffcebfaee93f843736e41475c1
frank <frank19781229@gmail.com>
2018-12-21 18:19:04 +0800

    增加网关路由（灰度发布）

--- api-gateway/src/main/java/com/cloud/console/UrlRedirectFilter.java
+++ api-gateway/src/main/java/com/cloud/console/UrlRedirectFilter.java
@@ -2,52 +2,42 @@ package com.cloud.console;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
+import io.jmnarloch.spring.cloud.ribbon.support.RibbonFilterContextHolder;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
 
 /** Created by Frank on 2018-12-10. */
-//@Component
+@Component
 public class UrlRedirectFilter extends ZuulFilter {
 
-  private static Map<String, String> urlMap = new HashMap<>();
+  @Override
+  public int filterOrder() {
+    return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+  }
 
-  static {
-    urlMap.put("t", "/user/");
+  @Override
+  public String filterType() {
+    return FilterConstants.PRE_TYPE;
+  }
+
+  @Override
+  public boolean shouldFilter() {
+    RequestContext ctx = RequestContext.getCurrentContext();
+    return !ctx.containsKey(FilterConstants.FORWARD_TO_KEY)
+        && !ctx.containsKey(FilterConstants.SERVICE_ID_KEY);
   }
 
   @Override
   public Object run() {
     RequestContext ctx = RequestContext.getCurrentContext();
     HttpServletRequest request = ctx.getRequest();
-    String url = request.getRequestURI();
-    String[] split = url.split("/", 3);
-    if (split.length >= 2) {
-      String val = urlMap.get(split[1]);
-      if (StringUtils.isNotEmpty(val)) {
-        url = url.replaceFirst("/" + split[1] + "/", val);
-        ctx.put(FilterConstants.REQUEST_URI_KEY, url);
-      }
+    String version = request.getHeader("version");
+    if (StringUtils.isNotBlank(version)) {
+      RibbonFilterContextHolder.getCurrentContext().add("version", version);
     }
     return null;
-  }
-
-  @Override
-  public boolean shouldFilter() {
-    return true;
-  }
-
-  @Override
-  public int filterOrder() {
-    return 1;
-  }
-
-  @Override
-  public String filterType() {
-    return FilterConstants.ROUTE_TYPE;
   }
 }
